7f5b3287da79619beae6d5a2518ef2f8d905132e
Snip885 <60302705+Snip885@users.noreply.github.com>
2021-01-11 01:36:16 +0300

    Улучшение синтаксиса

--- app/src/main/java/com/hfad/imtaus/MainActivity.java
+++ app/src/main/java/com/hfad/imtaus/MainActivity.java
@@ -2,109 +2,102 @@ package com.hfad.imtaus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.icu.number.Precision;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String finagling;
+    EditText growthEditText;
+    EditText weightEditText;
+    TextView outputDataTextView;
+    String textIMT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        growthEditText = (EditText) findViewById(R.id.growthEditText);
+        weightEditText = (EditText) findViewById(R.id.weightEditText);
+        outputDataTextView = (TextView) findViewById(R.id.outputDataTextView);
+
     }
 
-    public static double ocrug (double value, int stepen) {
-        double scale = Math.pow(10, stepen);
-        double valuefin = Math.ceil(value * scale) / scale;
-        return valuefin;
+    public static double rounding (double value, int degree) {
+        double scale = Math.pow(10, degree);
+        return Math.ceil(value * scale) / scale;
     }
 
-    private String getInterp(double fin) {
-        String interp;
-        if (fin < 18.5) {
-            interp = "Ниже нормального веса";
-        } else if ((fin >= 18.5) && (fin <= 25)) {
-            interp = "Нормальный вес";
-        } else if ((fin > 25) && (fin < 30)) {
-            interp = "Избыточный вес";
-        } else if ((fin >= 30) && (fin < 35)) {
-            interp = "Ожирение | степени";
-        } else if ((fin >= 35) && (fin < 40)) {
-            interp = "Ожирение || степени";
+    private String getIMT(double imt) {
+        String textIMT;
+        if (imt < 18.5) {
+            textIMT = getString(R.string.underWeight);
+        } else if ((imt >= 18.5) && (imt <= 25)) {
+            textIMT = getString(R.string.normalWeight);
+        } else if ((imt > 25) && (imt < 30)) {
+            textIMT = getString(R.string.preObesity);
+        } else if ((imt >= 30) && (imt < 35)) {
+            textIMT = getString(R.string.obesityClass1);
+        } else if ((imt >= 35) && (imt < 40)) {
+            textIMT = getString(R.string.obesityClass2);
         } else {
-            interp = "Ожирение ||| степени";
+            textIMT = getString(R.string.obesityClass3);
         }
-        return interp;
+        return textIMT;
     }
 
     public void onClickFindBeer(View view) {
 
-        EditText growth = (EditText) findViewById(R.id.rost);
-
-        EditText weight = (EditText) findViewById(R.id.mass);
-
-        if (growth.getText().toString().length() == 0 && weight.getText().toString().length() == 0){
-            growth.setError( "Введите свой рост в см." );
-            weight.setError("Введите свой вес в кг.");
-        } else if (weight.getText().toString().length() == 0) {
-            weight.setError("Введите свой вес в кг.");
-        } else if (growth.getText().toString().length() == 0) {
-            growth.setError( "Введите свой рост в см." );
+        if (growthEditText.getText().toString().length() == 0 && weightEditText.getText().toString().length() == 0){
+            growthEditText.setError(getString(R.string.enterGrowth));
+            weightEditText.setError(getString(R.string.enterWeight));
+        } else if (weightEditText.getText().toString().length() == 0) {
+            weightEditText.setError(getString(R.string.enterWeight));
+        } else if (growthEditText.getText().toString().length() == 0) {
+            growthEditText.setError(getString(R.string.enterGrowth));
         } else {
-
-            String interp;
-
-            String rost1 = growth.getText().toString();
+            String rost1 = growthEditText.getText().toString();
             double rost2 = Double.parseDouble(rost1);
 
-            String mass1 = weight.getText().toString();
+            String mass1 = weightEditText.getText().toString();
             double mass2 = Double.parseDouble(mass1);
 
-            TextView imt = (TextView) findViewById(R.id.imt);
+            TextView outputDataTextView = (TextView) findViewById(R.id.outputDataTextView);
 
-            DecimalFormat df = new DecimalFormat("##.#");
-            DecimalFormat form = new DecimalFormat("###.#");
+            DecimalFormat form = new DecimalFormat(".#");
 
             double rost3 = rost2/100;
 
-            double fin0 = (mass2 / (rost3 * rost3));
-            double fin = ocrug(fin0, 1);
+            double imt = rounding((mass2 / (rost3 * rost3)), 1);
 
             double minMass = (18.5*rost3*rost3);
             double maxMass = (25*rost3*rost3);
 
-            Log.d("Progress changed: ", "" + fin + " fin " + minMass);
+           // Log.d("Progress changed: ", "" + imt + " fin " + minMass);
 
-            interp = getInterp(fin);
+            textIMT = getIMT(imt);
 
-            imt.setText("Индекс массы тела: " + fin + " - " + interp + "\nНормальный вес при росте " + form.format(rost2) + " см должен находится " +
+            finagling = ("Индекс массы тела: " + imt + " - " + textIMT + "\nНормальный вес при росте " + form.format(rost2) + " см должен находится " +
                     "в пределах от " + form.format(minMass) + " до " + form.format(maxMass) + " кг");
 
+            outputDataTextView.setText(finagling);
+
+            }
         }
-    }
 
     public void onClickClear(View view) {
 
-        EditText growth = (EditText) findViewById(R.id.rost);
-        growth.getText().clear();
+        growthEditText.getText().clear();
 
-        EditText weight = (EditText) findViewById(R.id.mass);
-        weight.getText().clear();
+        weightEditText.getText().clear();
 
-        TextView imt = (TextView) findViewById(R.id.imt);
-        imt.setText("");
+        outputDataTextView.setText("");
 
     }
 
